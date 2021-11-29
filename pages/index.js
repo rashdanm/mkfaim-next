0d7543cdedcbf70a572dc53b5c62b9cde4ff3fcf
RashBM <57621825+RashBM@users.noreply.github.com>
2021-11-29 17:12:31 +0000

    clean up

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>MK Faim Photography</title>
+        <meta name="description" content="MK Faim by rashdan" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>hello</h1>
+    </div>
+  );
+}
