f8f73516ce008de4bfb241683f4b08c1b9680eee
RashBM <57621825+RashBM@users.noreply.github.com>
2021-11-30 18:04:50 +0000

    gallery lightbox

---
+++ data/galleryInfo.js
@@ -0,0 +1,119 @@
+const galleryInfo = [
+  {
+    id: 1,
+    imageNumber: 1,
+    title: "img1",
+  },
+  {
+    id: 2,
+    imageNumber: 2,
+    title: "img2",
+  },
+  {
+    id: 3,
+    imageNumber: 3,
+    title: "img3",
+  },
+  {
+    id: 4,
+    imageNumber: 4,
+    title: "img4",
+  },
+  {
+    id: 5,
+    imageNumber: 5,
+    title: "img5",
+  },
+  {
+    id: 6,
+    imageNumber: 6,
+    title: "img6",
+  },
+  {
+    id: 7,
+    imageNumber: 7,
+    title: "img7",
+  },
+  {
+    id: 8,
+    imageNumber: 8,
+    title: "img8",
+  },
+  {
+    id: 9,
+    imageNumber: 9,
+    title: "img9",
+  },
+  {
+    id: 10,
+    imageNumber: 10,
+    title: "img10",
+  },
+  {
+    id: 11,
+    imageNumber: 11,
+    title: "img11",
+  },
+  {
+    id: 12,
+    imageNumber: 12,
+    title: "img12",
+  },
+  {
+    id: 13,
+    imageNumber: 13,
+    title: "img13",
+  },
+  {
+    id: 14,
+    imageNumber: 14,
+    title: "img14",
+  },
+  {
+    id: 15,
+    imageNumber: 15,
+    title: "img15",
+  },
+  {
+    id: 16,
+    imageNumber: 16,
+    title: "img16",
+  },
+  {
+    id: 17,
+    imageNumber: 17,
+    title: "img17",
+  },
+  {
+    id: 18,
+    imageNumber: 18,
+    title: "img18",
+  },
+  {
+    id: 19,
+    imageNumber: 19,
+    title: "img19",
+  },
+  {
+    id: 20,
+    imageNumber: 20,
+    title: "img20",
+  },
+  {
+    id: 21,
+    imageNumber: 21,
+    title: "img21",
+  },
+  {
+    id: 22,
+    imageNumber: 22,
+    title: "img22",
+  },
+  {
+    id: 23,
+    imageNumber: 23,
+    title: "img23",
+  },
+];
+
+export default galleryInfo;
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,48 @@
+import styles from "../styles/Gallery.module.css";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import { SRLWrapper } from "simple-react-lightbox";
+import galleryInfo from "../data/galleryInfo";
+import Fade from "react-reveal/Fade";
+import Image from "next/image";
+import { imageConfigDefault } from "next/dist/server/image-config";
+
+const options = {
+  settings: {
+    autoplaySpeed: 2700,
+    overlayColor: "rgba(0, 0, 0, 0.95)",
+  },
+  buttons: {
+    iconColor: "rgba(255, 255, 255, 0.9)",
+    backgroundColor: "transparent",
+    showDownloadButton: false,
+  },
+  caption: {
+    showCaption: false,
+  },
+};
+
 const Gallery = () => {
   return (
     <div>
-      <h1>Gallery</h1>
+      <Navbar />
+      <SRLWrapper options={options}>
+        <div className="gallery-wrapper">
+          <div className="gallery-container">
+            {galleryInfo.map((item) => (
+              <div className="gallery-img" id={item.title}>
+                <img
+                  className="image"
+                  src={"/images/gallery/" + item.title + ".jpg"}
+                  alt="wedding"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </SRLWrapper>
+
+      <Footer />
     </div>
   );
 };
